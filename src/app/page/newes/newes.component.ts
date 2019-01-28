1b41227f7a9df813cd334e9b29a68006d488c0b4
yinweihong <1513372833@qq.com>
2019-01-28 17:58:51 +0800

    新闻更改功能

--- src/app/page/newes/newes.component.ts
+++ src/app/page/newes/newes.component.ts
@@ -15,6 +15,8 @@ export class NewesComponent implements OnInit {
   public author: string;
   public source: string;
   public newsList: Array<any>;
+
+  public newID: string;
   constructor(private http: HttpService) { }
 
   ngOnInit() {
@@ -44,25 +46,63 @@ export class NewesComponent implements OnInit {
   sub() {
     if (!this.title || this.title.length === 0) { alert('请输入标题'); return; }
     if (!this.content || this.content.length === 0) { alert('请输入内容'); return; }
-
-    this.http.post('/newAdd', {
-      title: this.title,
-      author: this.author,
-      content: this.content,
-      source: this.source
-    })
+    let p = {};
+    if (this.newID && this.newID.length > 0) {
+      p = {
+        id: this.newID,
+        title: this.title,
+        author: this.author,
+        content: this.content,
+        source: this.source
+      }
+    } else {
+      p = {
+        title: this.title,
+        author: this.author,
+        content: this.content,
+        source: this.source
+      }
+    }
+    this.http.post('/newAdd', p)
       .subscribe(e => {
         console.log(e);
         const a = (e['code'] === 200) ? '上传成功' : '上传失败';
         this.newGet();
+        this.newID = '';
+        this.title = '';
+        this.author = '';
+        this.content = '';
+        this.source = '';
         alert(a);
       });
+
   }
 
   newGet() {
     this.http.get('/newGet')
       .subscribe(r => {
         this.newsList = r['data'];
+      });
+  }
+
+  delet(id) {
+    this.http.get('/newDelete', { id })
+      .subscribe(r => {
+        alert('删除成功');
+        this.newsList = this.newsList.filter(a => a.id !== id);
+      });
+  }
+
+
+  edit(id) {
+    this.newID = id;
+    this.http.get('/newGet', { id })
+      .subscribe(r => {
+        console.log(r);
+        this.title = r['data']['0']['title'];
+        this.author = r['data']['0']['author'];
+        this.content = r['data']['0']['content'];
+        this.source = r['data']['0']['source'];
       });
   }
 }
